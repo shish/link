2f7eec665aaf500d3f9ba426063820c3fede7922
Shish <shish@shishnet.org>
2014-12-14 15:56:08 +0000

    much stuff

--- link.py
+++ link.py
@@ -16,18 +16,58 @@ urls = (
     '/?', 'surveys',
     '/survey', 'surveys',
     '/survey/(\d+)', 'survey',
+    '/compare/(\d+)/(.*)', 'compare',
     '/user', 'user',
+    '/login', 'login',
+    '/logout', 'logout',
+    '/create', 'create',
     '/(favicon.ico)', 'static',
 )
 site = "http://link.shishnet.org"
 
-app = web.application(urls, globals())
+
+def log_info(text):
+    if session.username:
+        logging.info("%s: %s" % (session.username, text))
+    else:
+        logging.info("<anon>: %s" % text)
 
 
 class LinkError(Exception):
     def __init__(self, title, message):
         self.title = title
         self.message = message
+
+
+def load_sqla(handler):
+    web.ctx.orm = scoped_session(sessionmaker(bind=engine))
+    try:
+        return handler()
+    except web.HTTPError:
+        web.ctx.orm.commit()
+        raise
+    except:
+        web.ctx.orm.rollback()
+        raise
+    finally:
+        web.ctx.orm.commit()
+        # If the above alone doesn't work, uncomment
+        # the following line:
+        #web.ctx.orm.expunge_all()
+
+
+def override_method(handler):
+    web.ctx.method = web.input().get("_method", web.ctx.method)
+    return handler()
+
+
+def if_logged_in(func):
+    def splitter(*args):
+        if session.username:
+            return func(*args)
+        else:
+            web.seeother("/")
+    return splitter
 
 
 def handle_exceptions(func):
@@ -43,22 +83,190 @@ def handle_exceptions(func):
     return logger
 
 
+class render_mako:
+    """copied from web.contrib.templates, with t.render_unicode"""
+    def __init__(self, *a, **kwargs):
+        from mako.lookup import TemplateLookup
+        self._lookup = TemplateLookup(*a, **kwargs)
+
+    def __getattr__(self, name):
+        # Assuming all templates are html
+        path = name + ".html"
+        t = self._lookup.get_template(path)
+        return t.render_unicode
+
+render = render_mako(
+    directories=["./templates/"],
+    input_encoding='utf-8',
+    output_encoding='utf-8',
+    default_filters=['unicode', 'h'],
+)
+app = web.application(urls, globals())
+app.add_processor(load_sqla)
+app.add_processor(override_method)
+
+import rediswebpy
+session = web.session.Session(
+    app, rediswebpy.RedisStore(),  # prefix='session:quotes:'),
+    initializer={'username': None})
+
+
 class surveys:
     @handle_exceptions
     def GET(self):
-        return "surveys"
+    	orm = web.ctx.orm
+        user = orm.query(User).filter(User.username==session.username).first()
+        surveys = orm.query(Survey)
+
+        if user:
+            nav = render.control(user)
+        else:
+            nav = render.login()
+        return render.standard("Interest Link", "List of Lists", nav, render.surveys(surveys))
 
 
 class survey:
     @handle_exceptions
+    @if_logged_in
     def GET(self, id):
-        return "survey %s" % id
+    	orm = web.ctx.orm
+        data = web.input()
+        user = orm.query(User).filter(User.username==session.username).one()
+        nav = render.control(user)
+
+        survey = orm.query(Survey).get(id)
+        response = orm.query(Response).filter(Response.survey==survey, Response.user==user).first()
+        compare = data.get("compareto")
+        if response:
+            others = orm.query(Response).filter(Response.survey==survey, Response.user!=user, Response.privacy=="public").all()
+            users = []
+            for other in others:
+                users.append(other.user)
+            nav = nav + render.others(survey, users)
+
+            if compare:
+                them = orm.query(User).filter(User.username==compare).one()
+                theirs = orm.query(Response).filter(Response.survey==survey, Response.user==them).first()
+                return render.standard("Survey", survey.name, nav, render.compare(survey, response, theirs))
+            else:
+                return render.standard("Survey", survey.name, nav, render.survey(survey, response))
+        else:
+            return render.standard("Survey", survey.name, nav, render.survey(survey, None, compareto=compare))
+
+    @handle_exceptions
+    @if_logged_in
+    def POST(self, id):
+    	orm = web.ctx.orm
+        post = web.input()
+        user = orm.query(User).filter(User.username==session.username).one()
+
+        survey = orm.query(Survey).get(id)
+        response = orm.query(Response).filter(Response.survey==survey, Response.user==user).first()
+        if response:
+            orm.delete(response)
+        response = Response(survey=survey, user=user)
+        if post.get("public") == "on":
+            response.privacy = "public"
+        else:
+            response.privacy = "private"
+
+        for q in survey.questions:
+            Answer(response=response, question=q, value=post["q%d" % q.id])
+
+        if post.get("compareto"):
+            web.seeother("/compare/%d/%s" % (survey.id, post.get("compareto")))
+        else:
+            web.seeother("/survey/%d" % survey.id)
+
+    @handle_exceptions
+    @if_logged_in
+    def DELETE(self, id):
+    	orm = web.ctx.orm
+        post = web.input()
+        user = orm.query(User).filter(User.username==session.username).one()
+
+        survey = orm.query(Survey).get(id)
+        response = orm.query(Response).filter(Response.survey==survey, Response.user==user).first()
+        if response:
+            orm.delete(response)
+        web.seeother("/survey/%d" % survey.id)
+
+class compare:
+    @handle_exceptions
+    @if_logged_in
+    def GET(self, id, compare):
+    	orm = web.ctx.orm
+        data = web.input()
+        user = orm.query(User).filter(User.username==session.username).one()
+        nav = render.control(user)
+
+        survey = orm.query(Survey).get(id)
+        response = orm.query(Response).filter(Response.survey==survey, Response.user==user).first()
+        if response:
+            others = orm.query(Response).filter(Response.survey==survey, Response.user!=user, Response.privacy=="public").all()
+            users = []
+            for other in others:
+                users.append(other.user)
+            nav = nav + render.others(survey, users)
+
+            them = orm.query(User).filter(User.username==compare).one()
+            theirs = orm.query(Response).filter(Response.survey==survey, Response.user==them).first()
+            return render.standard("Survey", survey.name, nav, render.compare(survey, response, theirs))
+        else:
+            web.seeother("/survey/%d?compareto=%s" % (survey.id, compare))
 
 
-class user:
+class login:
+    @handle_exceptions
+    def POST(self):
+        form = web.input()
+        username = str(form.username)
+        password = str(form.password)
+
+        user = web.ctx.orm.query(User).filter(User.username==username).first()
+        if user and user.check_password(password):
+            session.username = username
+            web.setcookie("username", username)
+            log_info("logged in from %s" % (web.ctx.ip))
+            web.seeother("/")
+        else:
+            raise LinkError("Error", "User not found")
+
+
+class logout:
     @handle_exceptions
     def GET(self):
-        return "user"
+        log_info("logged out")
+        session.kill()
+        web.seeother("/")
+
+
+class create:
+    @handle_exceptions
+    def POST(self):
+        form = web.input()
+        username = form.username
+        password1 = form.password1
+        password2 = form.password2
+        if len(form.email) > 0:
+            email = form.email
+        else:
+            email = None
+
+        user = web.ctx.orm.query(User).filter(User.username==username).first()
+        if user == None:
+            if password1 == password2:
+                user = User(username, password1, email)
+                web.ctx.orm.add(user)
+                web.ctx.orm.commit()
+
+                session.username = username
+                log_info("User created")
+                web.seeother("/")
+            else:
+                raise LinkError("Password Error", "The password and confirmation password don't match D:")
+        else:
+            raise LinkError("Name Taken", "That username has already been taken, sorry D:")
 
 
 class static:
--- models.py
+++ models.py
@@ -1,8 +1,9 @@
 import hashlib
+import bcrypt
 import StringIO
 
 from sqlalchemy import create_engine, func
-from sqlalchemy import Column, Integer, String, Unicode, Boolean, DateTime
+from sqlalchemy import Column, Integer, String, Unicode, Boolean, DateTime, Float
 from sqlalchemy import ForeignKey, Table
 from sqlalchemy.orm import relationship, backref
 
@@ -26,16 +27,140 @@ class User(Base):
 
     id       = Column(Integer, primary_key=True)
     username = Column(Unicode, nullable=False, index=True)
-    password = Column(String(32), nullable=False)
-    email    = Column(Unicode, default=u"")
+    password = Column(String(255), nullable=False)
+    email    = Column(Unicode, default=None, nullable=True)
 
-    def __init__(self, username, password, email):
+    def __init__(self, username, password, email=None, password_crypt=None):
         self.username = username
-        self.password = hashlib.md5("replace-with-bcrypt-"+username.lower()+password).hexdigest()
+        self.password = bcrypt.hashpw(password, bcrypt.gensalt())
         self.email = email
+        if password_crypt:
+            self.password = password_crypt
+
+    def check_password(self, password):
+        return bcrypt.hashpw(password, self.password) == self.password
 
     def __repr__(self):
         return "<User('%s')>" % (self.name, )
+
+
+class Survey(Base):
+    __tablename__ = "survey"
+
+    id       = Column(Integer, primary_key=True)
+    name     = Column(String, unique=True)
+    description     = Column(Unicode, nullable=False)
+
+    def set_questions(self, qs):
+        for q in qs:
+            self.questions.append(q)
+            if q.flip:
+                self.questions.append(q.flip)
+
+
+class Question(Base):
+    __tablename__ = "question"
+
+    id        = Column(Integer, primary_key=True)
+    survey_id = Column(Integer, ForeignKey('survey.id'), nullable=False, index=True)
+    flip_id   = Column(Integer, ForeignKey('question.id'), nullable=True, index=True)
+    order     = Column(Float, nullable=False, default=0)
+    text      = Column(Unicode, nullable=False)
+    extra     = Column(Unicode, nullable=True)
+
+    survey = relationship(
+        "Survey",
+        backref=backref('questions', order_by=[order.asc(), id.asc()])
+    )
+    flip = relationship(
+        "Question", remote_side=[id], post_update=True,
+    )
+
+    def __init__(self, text, flip_text=None, extra=None):
+        self.text = text
+        if flip_text:
+            self.flip = Question(flip_text)
+            self.flip.flip = self
+        self.extra = extra
+
+    def matches(self, other):
+        if self.flip:
+            return self.flip == other
+        else:
+            return self == other
+
+
+class Response(Base):
+    __tablename__ = "response"
+
+    id       = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False, index=True)
+    survey_id = Column(Integer, ForeignKey('survey.id'), nullable=False, index=True)
+    privacy  = Column(String, nullable=False, default="private")
+
+    user = relationship(
+       "User",
+        backref=backref('responses', cascade="all")
+    )
+    survey = relationship(
+       "Survey",
+        backref=backref('responses', cascade="all")
+    )
+
+    def value(self, question_id):
+        for a in self.answers:
+            if a.question.id == question_id:
+                return a.value
+        return 0
+
+
+class Answer(Base):
+    __tablename__ = "answer"
+
+    id       = Column(Integer, primary_key=True)
+    question_id = Column(Integer, ForeignKey('question.id'), nullable=False, index=True)
+    response_id = Column(Integer, ForeignKey('response.id'), nullable=False, index=True)
+    value = Column(Integer, nullable=True)
+
+    question = relationship(
+       "Question",
+	backref=backref("answers", cascade="all", order_by=[id.asc()])
+    )
+    response = relationship(
+       "Response",
+        backref=backref('answers', cascade="all")
+    )
+
+    def value_name(self):
+        return {
+            -2: "do not want",
+            -1: "do not want",
+            0: "don't care about",
+            1: "would try",
+            2: "like",
+        }[self.value]
+
+
+class Key(Base):
+    __tablename__ = "key"
+
+    id       = Column(Integer, primary_key=True)
+    key      = Column(String, unique=True)
+    owner_id = Column(Integer, ForeignKey('user.id'), nullable=False, index=True)
+    survey_id = Column(Integer, ForeignKey('survey.id'), nullable=False, index=True)
+    viewer_id = Column(Integer, ForeignKey('user.id'), nullable=True, index=True)
+
+    owner = relationship(
+       "User", foreign_keys=[owner_id],
+    )
+    survey = relationship(
+       "Survey",
+    )
+    viewer = relationship(
+       "User", foreign_keys=[owner_id],
+    )
+
+
 
 
 metadata = Base.metadata
