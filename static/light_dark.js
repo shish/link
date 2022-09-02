d8c4cdbc13e08c988ee44e7a7d835389d1788a8b
djahren <djahren@djahren.com>
2022-09-01 23:22:14 -0700

    🎨 formatting

--- static/light_dark.js
+++ static/light_dark.js
@@ -1,13 +1,13 @@
 function isDarkModeEnabled() {
-    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 }
 
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => { //scheme updated
-    setColorMode() 
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => { //system scheme updated
+    setColorMode();
 });
 
 function setColorMode(){
-    document.body.className = isDarkModeEnabled() ? "dark" : "light"
+    document.body.className = isDarkModeEnabled() ? "dark" : "light";
 }
 
-setColorMode() //set the color mode class as soon as the body tag is loaded
+setColorMode(); //set the color mode class as soon as the body tag is loaded
--- static/script.js
+++ static/script.js
@@ -5,9 +5,9 @@ $(function () {
     $("#jscontrols").append($("<label><input type='checkbox' id='mark'> Highlight unresponded questions</label>"));
     $("#mark").click(function (evt) {
         if ($("#mark").is(":checked")) {
-            $("table.zebra")[0].classList.add("highlight-unmarked")
+            $("table.zebra").addClass("highlight-unmarked");
         } else {
-            $("table.zebra")[0].classList.remove("highlight-unmarked")
+            $("table.zebra").removeClass("highlight-unmarked");
         }
     });
 });
